14f5cafbdc854fdc46632e5f2646af21e5556410
Toan Le <thanhtoanhcm1602@gmail.com>
2024-07-09 14:17:51 +0700

    Type và Interface.

--- src/count.tsx
+++ src/count.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-export default function Count({ count, setCount }: { count: number; setCount: React.Dispatch<React.SetStateAction<number>> }) {
+// type Props = { count: number; setCount: React.Dispatch<React.SetStateAction<number>> };
+interface IProps {
+  count: number;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function Count({ count, setCount }: IProps) {
   return (
     <div className="card">
       <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
